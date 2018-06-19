82aa1e8dfe9e91f409e67261b9374187abe299d7
나현석 <useki@naver.com>
2018-06-19 22:54:55 +0900

    test

--- app/src/main/java/com/nycompany/skyban/LoginActivity.kt
+++ app/src/main/java/com/nycompany/skyban/LoginActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    //test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
