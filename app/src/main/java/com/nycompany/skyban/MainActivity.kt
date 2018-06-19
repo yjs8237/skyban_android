15f5b60a811b1d0f3c2cd933d9184c8a9a0e1cf6
pc-PC\pc <useki@naver.com>
2018-06-19 19:00:33 +0900

    ui dev ing

--- app/src/main/java/com/nycompany/skyban/MainActivity.kt
+++ app/src/main/java/com/nycompany/skyban/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.nycompany.skyban
 
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import com.nycompany.skybanminitp.FragmentsAvailable
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+    private var currentFragment:FragmentsAvailable? = null
+    private var fragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            changeCurrentFragment(FragmentsAvailable.INORDER, getIntent().extras)
+        } else{
+            currentFragment = savedInstanceState.getSerializable("currentFragment") as FragmentsAvailable
+        }
+    }
+
+    private fun changeCurrentFragment(newFragmentType: FragmentsAvailable?, extras: Bundle?) {
+        fragment = null
+        when(newFragmentType){
+            FragmentsAvailable.INORDER -> fragment = InorderFragment()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
