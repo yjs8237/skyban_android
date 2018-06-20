305daa125dab3d5d1ce9179d91bba52c66fc9919
pc-PC\pc <useki@naver.com>
2018-06-20 13:21:51 +0900

    ui dev ing

--- app/src/main/java/com/nycompany/skyban/LoginActivity.kt
+++ app/src/main/java/com/nycompany/skyban/LoginActivity.kt
@@ -5,14 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 
+
 class LoginActivity : AppCompatActivity() {
-    //test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        button.setOnClickListener{
-            startActivity(Intent().setClass(this, MainActivity::class.java))
+        loginBtn.setOnClickListener{
+            startActivity(Intent().setClass(this, termsActivity::class.java))
             finish()
         }
     }
---
+++ app/src/main/java/com/nycompany/skyban/termsActivity.kt
@@ -0,0 +1,21 @@
+package com.nycompany.skyban
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_terms.*
+
+class termsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms)
+
+        backBtn.setOnClickListener{
+            finish()
+        }
+
+        agrrBtn.setOnClickListener{
+
+        }
+    }
+}
